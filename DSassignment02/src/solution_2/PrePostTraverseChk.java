d0d4651dfd09ca525be51c04dfd107a1c9bcc645
DELL <DELL@DESKTOP-PNBGS9O>
2020-03-28 21:31:49 +0530

    create class PrePostTraverse chk to check condition

---
+++ DSassignment02/src/solution_2/PrePostTraverseChk.java
@@ -0,0 +1,54 @@
+package solution_2;
+public class PrePostTraverseChk {
+	TreeNode root;
+	public TreeNode getRoot() {
+		return root;
+	}
+	public void insert(int data) {
+		TreeNode node=new TreeNode(data);
+		if(root==null) {
+			root=node;
+		}
+		else {
+			TreeNode temp=root;
+			TreeNode parent=null;
+			while(temp!=null) {
+				parent=temp;
+				if(data<=temp.getData()) {
+					temp=temp.getLeft();
+				}
+					else {
+						temp=temp.getRight();
+					}
+				}
+			if(node.getData()<=parent.getData()) {
+				parent.setLeft(node);
+			}
+				else {
+					parent.setRight(node);
+				}
+			}
+			
+			}
+	
+void traversePostOrder(TreeNode node) {
+	if(node!=null) {
+		
+		
+		traversePostOrder(node.getLeft());
+		
+		traversePostOrder(node.getRight());
+		System.out.print(node.getData()+" ");
+	}
+}
+void traversePreOrder(TreeNode node) {
+	if(node!=null) {
+		System.out.print(node.getData()+" ");
+		
+		traversePreOrder(node.getLeft());
+		
+		traversePreOrder(node.getRight());
+	}
+
+}	
+}
